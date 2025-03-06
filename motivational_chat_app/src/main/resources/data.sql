585959121a3558b8fd518080aed867a1520512c0
yolonime <ti.boug.95100@gmail.com>
2025-03-06 15:58:30 +0100

    finalisation du front

--- motivational_chat_app/src/main/resources/data.sql
+++ motivational_chat_app/src/main/resources/data.sql
@@ -1,5 +1,4 @@
-INSERT INTO quote (author, text) VALUES ('Albert Einstein', 'La créativité, c’est l’intelligence qui s’amuse.');
-INSERT INTO quote (author, text) VALUES ('Steve Jobs', 'Stay hungry, stay foolish.');
-INSERT INTO quote (author, text) VALUES ('Nelson Mandela', 'Cela semble toujours impossible, jusqu’à ce qu’on le fasse.');
-INSERT INTO quote (author, text) VALUES ('Albert Einstein', 'La logique vous mènera d’un point A à un point B. L’imagination vous mènera partout.');
-INSERT INTO quote (author, text) VALUES ('Steve Jobs', 'Votre temps est limité, ne le gâchez pas en menant une existence qui n’est pas la vôtre.');
+INSERT INTO quote (author, text) VALUES ('Maya Angelou', 'Vous ne pouvez pas épuiser votre créativité. Plus vous l’utilisez, plus vous en avez.');
+INSERT INTO quote (author, text) VALUES ('Elon Musk', 'Lorsque quelque chose est suffisamment important, vous le faites même si les chances ne sont pas en votre faveur.');
+INSERT INTO quote (author, text) VALUES ('Isaac Newton', 'Les hommes construisent trop de murs et pas assez de ponts.');
+INSERT INTO quote (author, text) VALUES ('Marie Curie', 'Dans la vie, rien n’est à craindre, tout est à comprendre.');
